572c5f97eec95b57999c713863d3bb363ae69346
Gabriel Soares <gabrielalcantarars@gmail.com>
2025-10-27 20:00:12 -0300

    feat: Base structure

---
+++ src/main/java/fiap_adj8/feedback_platform/feedback_app/FiapAdj8FeedbackAppApplication.java
@@ -0,0 +1,13 @@
+package fiap_adj8.feedback_platform.feedback_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapAdj8FeedbackAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiapAdj8FeedbackAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fiap_adj8/feedback_platform/feedback_app/FiapAdj8FeedbackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package fiap_adj8.feedback_platform.feedback_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapAdj8FeedbackAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
